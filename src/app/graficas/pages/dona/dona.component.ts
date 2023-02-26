f88a4b79547cc7a90b07822a4c7eea767dd7ea0c
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-02-26 20:43:22 -0300

    grafica de donas

--- src/app/graficas/pages/dona/dona.component.ts
+++ src/app/graficas/pages/dona/dona.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ChartData, ChartType, Color, ChartConfiguration } from 'chart.js';
 
 @Component({
   selector: 'app-dona',
@@ -7,5 +8,26 @@ import { Component } from '@angular/core';
   ]
 })
 export class DonaComponent {
+  // Doughnut
+  public doughnutChartLabels: string[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
+  public doughnutChartData: ChartData<'doughnut'> = {
+    labels: this.doughnutChartLabels,
+
+    datasets: [
+      {
+        data: [350, 450, 100],
+        backgroundColor: [
+          '#8338ec',
+          '#ff006e',
+          '#ffd60a'
+        ],
+      },
+
+    ]
+  };
+  public doughnutChartType: ChartType = 'doughnut';
+  // public barChartColors: Color[] = [
+  //   'red', 'green', 'blue'
+  // ]
 
 }
