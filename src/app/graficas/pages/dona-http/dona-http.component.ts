b5657cd611b9b0698906c9ced4c81ecf2e569e1c
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-02-26 21:44:32 -0300

    mostrar info de api

--- src/app/graficas/pages/dona-http/dona-http.component.ts
+++ src/app/graficas/pages/dona-http/dona-http.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GraficasService } from '../../services/graficas.service';
+import { ChartData, ChartDataset, ChartType } from 'chart.js';
 
 @Component({
   selector: 'app-dona-http',
@@ -6,6 +8,42 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class DonaHttpComponent {
+export class DonaHttpComponent implements OnInit {
+
+  constructor(private graficasService: GraficasService) { }
+
+  ngOnInit(): void {
+
+    this.graficasService.getUsuariosRedesSociales()
+      .subscribe(data => {
+        console.log(data)
+        const labels = Object.keys(data);
+        const values = Object.values(data);
+        this.doughnutChartLabels.push(...labels);
+        this.doughnutChartDatasets.push({data:values})
+      })
+
+
+  }
+
+  public doughnutChartLabels: string[] = [
+    // 'Download Sales', 'In-Store Sales', 'Mail-Order Sales'
+  ];
+  public doughnutChartDatasets: ChartDataset<"doughnut", number[]>[] = [
+    // {
+    //   data: [350, 450, 100],
+    //   backgroundColor: [
+    //     '#8338ec',
+    //     '#ff006e',
+    //     '#ffd60a'
+    //   ],
+    // },
+
+  ];
+  public doughnutChartData: ChartData<'doughnut'> = {
+    labels: this.doughnutChartLabels,
+    datasets: this.doughnutChartDatasets,
+  };
+  public doughnutChartType: ChartType = 'doughnut';
 
 }
